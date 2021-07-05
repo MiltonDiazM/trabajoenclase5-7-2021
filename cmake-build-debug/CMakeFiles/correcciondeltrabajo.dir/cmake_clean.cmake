1d9b715f255ff38ee2917e83769cd94f3b58ebc2
Milton Diaz <diazmorcillomilton@gmail.com>
2021-07-05 16:38:23 -0500

    correccion y trabajo en clase

---
+++ cmake-build-debug/CMakeFiles/correcciondeltrabajo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correcciondeltrabajo.dir/main.cpp.obj"
+  "correcciondeltrabajo.exe"
+  "correcciondeltrabajo.exe.manifest"
+  "correcciondeltrabajo.pdb"
+  "libcorrecciondeltrabajo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correcciondeltrabajo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
